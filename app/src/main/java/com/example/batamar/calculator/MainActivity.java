98b46c70fa8a2499ef5a2cb37cbc3897884e9fb7
Artem <metema7@gmail.com>
2017-02-07 20:28:39 +0300

    v.1

---
+++ app/src/main/java/com/example/batamar/calculator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.batamar.calculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    static final String EXTRA_FIRST_NUMBER = "first_number";
+    static final String EXTRA_SECOND_NUMBER = "second_number";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.addition);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Result.class);
+                EditText editText = (EditText) findViewById(R.id.first_number);
+                if(editText.getText().toString().equals("")){
+                    editText.setError(getString(R.string.error_empty));
+                    return;
+                }
+                intent.putExtra(EXTRA_FIRST_NUMBER, editText.getText().toString());
+                editText = (EditText) findViewById(R.id.second_number);
+                if(editText.getText().toString().equals("")){
+                    editText.setError(getString(R.string.error_empty));
+                    return;
+                }
+                intent.putExtra(EXTRA_SECOND_NUMBER, editText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/batamar/calculator/Result.java
@@ -0,0 +1,30 @@
+package com.example.batamar.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        String firstNumber = getIntent().getStringExtra(MainActivity.EXTRA_FIRST_NUMBER);
+        String secondNumber = getIntent().getStringExtra(MainActivity.EXTRA_SECOND_NUMBER);
+
+        TextView textView = (TextView) findViewById(R.id.text_result);
+        textView.setText(generateCalculatedString(firstNumber,secondNumber));
+    }
+
+    private String generateCalculatedString(String firstNumber, String secondNumber){
+        String string = firstNumber + " + ";
+        if(secondNumber.matches("^-\\d+$")) string = string + "( " + secondNumber + " )";
+        else string += secondNumber;
+        string = string + " = " + (Integer.valueOf(firstNumber) + Integer.valueOf(secondNumber));
+        return string;
+    }
+}
